bdc068bcdf74b4570e8b4a668f8551cfa41646ae
SeraTanin <t.ser1982@mail.ru>
2023-11-21 18:03:21 +0300

    First commit

--- 00.py
+++
@@ -1,3 +0,0 @@
-from datetime import datetime as dt
-now = dt.now()
-print(f"Time {now:%d,%m,%Y %H:%M}")
--- main.py
+++ main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
+import telebot
+from telebot import types
+import os
+from dotenv import load_dotenv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
+name = ''
+surname = ''
+age = 0
+API_TOKEN = os.getenv('API_TOKEN')
+link_name = os.getenv('link_name')
+bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Which band is the best?")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    if message.text == 'Hello':
+        bot.reply_to(message, 'Hello, how are you?')
+    elif message.text == '/reg':
+        bot.send_message(message.from_user.id, 'Hi, what is your name?')
+        bot.register_next_step_handler(message, reg_name)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # bot.reply_to(message, message.text)
+
+
+def reg_name(message):
+    global name
+    name = message.text
+    bot.send_message(message.from_user.id, 'What is your surname?')
+    bot.register_next_step_handler(message, reg_surname)
+
+
+def reg_surname(message):
+    global surname
+    surname = message.text
+    bot.send_message(message.from_user.id, 'What is your age?')
+    bot.register_next_step_handler(message, reg_age)
+
+
+def reg_age(message):
+    global age
+
+    while age == 0:
+        try:
+            age = int(message.text)
+        except Exception:
+            bot.send_message(message.from_user.id, 'Enter a number please')
+
+    # make a buttons yes and no
+    keyboard = types.InlineKeyboardMarkup()
+    key_yes = types.InlineKeyboardButton(text='Yes', callback_data='Yes')
+    keyboard.add(key_yes)
+
+    key_no = types.InlineKeyboardButton(text='No', callback_data='No')
+    keyboard.add(key_no)
+    question = f"Are you {age} years old? Is your name {name} and surname is {surname}?"
+    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == 'Yes':
+        bot.send_message(call.message.chat.id, "Nice to meet you")
+    elif call.data == 'No':
+        bot.send_message(call.message.chat.id, "Can you write this correctly?")
+        bot.send_message(call.message.chat.id, "What is your name?")
+        bot.register_next_step_handler(call.message, reg_name)
+
+
+bot.infinity_polling()
